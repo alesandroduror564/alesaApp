befa78f687ef6a1729c7080b0d34fb342bcaadf8
alesandroduror564 <147962316+alesandroduror564@users.noreply.github.com>
2025-07-12 06:40:59 +0200

    Create transaction-detail-item.stories.js

---
+++ ui/pages/confirmations/components/transaction-detail-item/transaction-detail-item.stories.js
@@ -0,0 +1,3 @@
+import React from 'import { Icon, IconName } from '../../components/component-InfoTooltip';
+import TransactionDetailItem from './TransactionDetailItem';
+export default {title: 'Confirmations/Components/TransactionDetailItem',component: TransactionDetail'detailTitle':{control:'object'}detailText:{control:'text'}detail}exportconstDefaultStory =()=>({})}default.args={detailTitle:(<><h1>Confirm transaction</h1><Tooltiptext='tooltip'position='top'icon={Icon.Info} />'</>)} <h1>Transfer of funds</h1><TransactionDetail {...args} /></div>);default.args={}; 
